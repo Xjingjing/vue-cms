8105d7c0b727db6da4230496e24dd71ceb45d44c
wj <1354048405@qq.com>
2017-11-29 14:38:25 +0800

    项目搭建&头部样式完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-      <router-view/>
+    <mt-header fixed title="固定在顶部">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+    </mt-header>
+
+    <router-view/>
+
+    
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from "./App.vue"
 
 import router from "./routers/router.js"
 
+import MintUI from "mint-ui"
+import "mint-ui/lib/style.css"
+Vue.use(MintUI);
+
 var vm = new Vue({
     el: "#app",
     router,
